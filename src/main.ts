65b4d2c8b3c8749c0e3009fb2727445aa0b1af05
vickysomtee <vickysomtee@gmail.com>
2024-12-27 22:35:18 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.PORT);
-  await app.listen(process.env.PORT);
+  console.log(process.env.APP_PORT);
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
